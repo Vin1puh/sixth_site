a115fea3904fab0663b618ef5ffc51b3898bedb6
V1nipuh <150341348+Vin1puh@users.noreply.github.com>
2024-12-24 21:11:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper-container', {
+    navigation: {
+        pagination:{
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+
+        }
+    }
+})
